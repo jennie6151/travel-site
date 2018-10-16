30fdf6eb337299232c1d5cab0e5a197fac134942
Jennifer Dick <jennie6151@hotmail.com>
2018-10-16 13:54:35 +0000

    search results added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -7,56 +7,65 @@ function initAutocomplete() {
         zoom: 3,
         center: mapCenter,
     });
-    
-    var input = document.getElementById('pac-input');
+
+    var input = document.getElementById('mapSearchInput');
     var searchBox = new google.maps.places.SearchBox(input);
     map.controls[google.maps.ControlPosition.CENTER].push(input);
 
     map.addListener('bounds_changed', function() {
-      searchBox.setBounds(map.getBounds());
+        searchBox.setBounds(map.getBounds());
     });
-    
+
     var markers = [];
-searchBox.addListener('places_changed', function() {
-  var places = searchBox.getPlaces();
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
 
-  if (places.length == 0) {
-    return;
-  }
+        if (places.length == 0) {
+            return;
+        }
 
-  markers.forEach(function(marker) {
-    marker.setMap(null);
-  });
-  markers = [];
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+        
+var resultsList = document.getElementById('searchResults');
+resultsList.innerHTML = '';
 
-  var bounds = new google.maps.LatLngBounds();
-  places.forEach(function(place) {
-    if (!place.geometry) {
-      console.log("Returned place contains no geometry");
-      return;
-    }
-    var icon = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    };
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
 
-    markers.push(new google.maps.Marker({
-      map: map,
-      icon: icon,
-      title: place.name,
-      position: place.geometry.location
-    }));
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
 
-    if (place.geometry.viewport) {
-  
-      bounds.union(place.geometry.viewport);
-    } else {
-      bounds.extend(place.geometry.location);
-    }
-  });
-  map.fitBounds(bounds);
-});
+            
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(place.name));
+            resultsList.appendChild(li);
+
+            if (place.geometry.viewport) {
+
+                bounds.union(place.geometry.viewport);
+            }
+            else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
 }
