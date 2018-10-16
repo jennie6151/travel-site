af95bbe1baae6441ea398ba4c77269fce23d761f
Jennifer Dick <jennie6151@hotmail.com>
2018-10-16 12:47:35 +0000

    Autocomplete input added

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,45 +1,20 @@
 var map;
-        var infowindow;
+var infowindow;
 
-        function createMap() {
-            var mapCenter = new google.maps.LatLng(51.508710, -0.075799);
+function initAutocomplete() {
+    var mapCenter = new google.maps.LatLng(51.508710, -0.075799);
+    map = new google.maps.Map(document.getElementById('googleMap'), {
+        zoom: 15,
+        center: mapCenter,
+    });
+    
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
-            map = new google.maps.Map(document.getElementById('googleMap'), {
-                zoom: 15,
-                center: mapCenter,
+    map.addListener('bounds_changed', function() {
+      searchBox.setBounds(map.getBounds());
+    });
 
-            });
-
-            infowindow = new google.maps.InfoWindow();
-            var service = new google.maps.places.PlacesService(map);
-            service.nearbySearch({
-                location: mapCenter,
-                type: ['airport, amusement_park, aquarium, art_gallery, bar, bowling_alley, bus_station, cafe, campground, car_rental, casino, cemetery, church, city_hall, courthouse, embassy, hindu_temple, library, mosque, movie_theater, museum, night_club, park, restaurant, rv_park, shopping_mall, spa, stadium, subway_station, synagogue, taxi_stand, train_station, transit_station, zoo'],
-                radius: '1000'
-            }, processSearchResults);
-        }
-
-        function processSearchResults(results, status) {
-            if (status === google.maps.places.PlacesServiceStatus.OK) {
-                for (var i = 0; i < results.length; i++) {
-                    createMarker(results[i]);
-                }
-            }
-        }
-
-        function createMarker(place) {
-            var placeLoc = place.geometry.location;
-            var marker = new google.maps.Marker({
-                map: map,
-                position: place.geometry.location,
-                icon: "/assets/images/map-pin-blue.png"
-            });
-
-
-            google.maps.event.addListener(marker, 'click', function() {
-                infowindow.setContent(place.name);
-                infowindow.open(map, this);
-            });
-
-
-        }
+ 
+  }
